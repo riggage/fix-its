d8c5f626e46e987135db5405cc37ece4c7ff86a4
Fluffy-Lion <leoncooper31@gmail.com>
2021-09-28 11:34:45 +0100

    fix-it challenges added

---
+++ src/Fix-it/Fix-it-2.js
@@ -0,0 +1,12 @@
+const FixIt2 = () => {
+    return 
+        <div>
+            <h1>FIX IT 2</h1>
+            <ul>
+                <li>blue</li>
+                <li>green</li>
+                <li>red</li>
+            <ul>
+        </div>
+
+}
---
+++ src/Fix-it/Fix-it-3.js
@@ -0,0 +1,13 @@
+const Fixit3 = () => {
+    return (
+        <h1>Fix it 3</h1>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+          tempor incididunt ut labore et dolore magna aliqua. Vulputate ut
+          pharetra sit amet aliquam id diam.
+        </p>
+      )
+  };
+  
+  export default Fixit3;
+  
