3ef8627514f37fd887d73f3e99b982ce61626f52
Fluffy-Lion <leoncooper31@gmail.com>
2021-09-28 10:28:51 +0100

    first commit, project set-up

---
+++ src/Fix-it-1/Fix-it-1.js
@@ -0,0 +1,9 @@
+
+
+const FixIt1 = () => {
+    return (
+        <h1>FIX IT 1</h1>
+    )
+}
+
+export default FixIt1
--- src/index.js
+++ src/index.js
@@ -1,17 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import FixIt1 from './Fix-it-1/Fix-it-1';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+ReactDOM.render(<App />,document.getElementById('root'));
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// FIX-IT-1
+// ReactDOM.render(<FixIt1 />,document.getElementById('root'));
+
+// FIX-IT-2
+// ReactDOM.render(<FixIt1 />,document.getElementById('root'));
